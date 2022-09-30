1e128ccc80c8c092bab525f42f73fc0a24db2ce9
thirureddy-git <reachreddy@gmail.com>
2022-09-29 19:08:01 -0500

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Menu';
+  title = 'Chirpus Pizza!';
+Menu:MenuItem[] = [
+    {pizzaname:"Veggie Lovers", type:"Pizza", price:12.99},
+    {pizzaname:"Chicken Supreme", type:"Pizza", price:14.99},
+    {pizzaname:"Pepperoni ", type:"Pizza", price:14.99},
+    {pizzaname:"Veggie Delight", type:"Pizza", price:14.99},
+    {pizzaname:"Lava Cake", type:"Desert", price:4.99},
+    {pizzaname:"Double Cheese", type:"Pizza", price:11.99},
+    {pizzaname:"Cheese Sticks", type:"Sides", price:5.99}
+ ]
 }
---
+++ src/app/menu-item.ts
@@ -0,0 +1,5 @@
+export interface MenuItem {
+    pizzaname:string;
+    type:string;
+    price:number;
+}
